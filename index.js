4f81aab5ea57fc4439b1f2dbfd477ac09d411a37
BITU <mandeep.bitu.13@gmail.com>
2022-06-29 18:40:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var hbs = require('hbs');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var socket = require('socket.io');
+
+var PORT = process.env.PORT || 8000;
+var app = express();
+
+
+app.set("view engine","hbs")
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static(__dirname+"/views"))
+
+//http req type end point....
+app.get('/',(req,res)=>{
+  res.render("index.hbs")
+})
+
+const server = app.listen(PORT,async (req,res)=>{
+  console.log(`server listen @ ${PORT}`)
+})
+
+//setup socket.....
+var io = socket(server);
+
+io.on('connection',(socket)=>{
+    console.log("socket connection is working",socket.id)
+    
+    //reciving data from any of the node that connected to this server...
+    socket.on('chat',(data)=>{
+
+        //after reciving data, emit data to all other nodes that are connected to the server...
+        io.sockets.emit('chat',data);
+    })
+
+
+    //broadCasting
+    //after reciving data, emit/send data to all other nodes that are connected to the server,except the node from which it will get the data...
+    socket.on("typing",(data)=>{
+        socket.broadcast.emit("typing",data)
+    })
+    
+    //broadcasting when input text is null
+    socket.on("null-typ",()=>{
+        socket.broadcast.emit("null-typ",{})
+    })
+})
---
+++ views/chat.js
@@ -0,0 +1,105 @@
+//make connection
+var socket = io.connect('http://localhost:8000');
+
+//ui-opration
+
+var input = document.getElementById('ctxt')
+var btn = document.getElementById("send")
+var chatBox = document.getElementsByClassName('chat-box')[0]
+const feedBack = document.getElementById('feedback')
+var owner;
+
+
+
+//taking ownwer name.....
+
+while(owner ==+ null || owner === "" ||owner === undefined){
+    owner = prompt("enter your name");
+}
+document.getElementsByClassName('main-heading')[0].children[1].innerText = owner;
+
+var Socket_id;
+socket.on('connect',()=>{
+    Socket_id = socket.id
+    console.log(Socket_id)
+})
+
+
+// emit event (reciving emit msg from serever)
+var data = {owner:owner, msg:"",id:""}
+btn.addEventListener('click',function(){
+    data.id = Socket_id
+    data.msg = input.value
+    input.value = "";
+
+    //emiting the msg
+    socket.emit('chat',data);
+
+},false)
+
+// input.addEventListener('keyup',(e)=>{
+    
+// })
+
+input.addEventListener('keyup',(e)=>{
+    if(e.key==='Enter'){
+        data.id = Socket_id
+        data.msg = input.value
+        input.value = "";
+
+        //emiting the msg
+        socket.emit('chat',data);
+    }
+    // if(input.value === "" || input.value === null){
+    //     socket.emit("null-typ",{});
+    // }
+    // else{
+    //     socket.emit("typing",owner)
+    // }
+},false)
+
+
+
+
+//listen for event
+socket.on('chat',(data)=>{
+
+    if(data.id === Socket_id){
+        // feedBack.style.visibility = "hidden";
+        chatBox.innerHTML += `
+                    <div class="chat-sub-box owner-chat">
+                        <div class="sender-head">
+                            You
+                        </div>
+                        <div class="txt">
+                            ${data.msg}
+                        </div>
+                    </div>`
+    }else{
+        // feedBack.style.visibility = "hidden";
+        chatBox.innerHTML += `
+                    <div class="chat-sub-box">
+                        <div class="sender-head">
+                            ${data.owner}
+                        </div>
+                        <div class="txt">
+                            ${data.msg}
+                        </div>
+                    </div>`
+    }
+
+    chatBox.scrollTo(0, chatBox.scrollHeight)
+})
+
+// socket.on('typing',(data)=>{
+//     //console.log("reciving typing result")
+//      console.log(feedBack);
+//     feedBack.style.visibility = "visible"
+//     feedBack.innerText = data + " is typing...";
+// })
+
+// socket.on("null-typ",()=>{
+//     //console.log('reciving-null-typ result')
+//     console.log(feedBack);
+//     feedBack.style.visibility = "hidden"
+// })
